1e22f13f6c44cf6584267320122e7dffe459631e
JustDevWeb <zm91672@gmail.com>
2023-05-17 23:01:39 +0800

    fix:number divided task

--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -2,7 +2,6 @@ package lang.print.gaps.task5;
 
 public class ThreeStuckVars {
     public static void main(String[] args) {
-
         int first = 1;
         int second = 10;
         int third = 100;
